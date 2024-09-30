04dc9b1ab454946faf8d8d9fc9e4382fb2d4ca02
Caio Ramos <caioramos97@gmail.com>
2024-09-30 12:40:11 -0300

    Update solic cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_30_130453) do
+ActiveRecord::Schema[7.2].define(version: 2023_11_09_162035) do
   create_table "feeds", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
@@ -64,17 +64,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_30_130453) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["selection_type", "selection_id"], name: "index_settings_on_selection"
-  end
-
-  create_table "solid_cache_entries", force: :cascade do |t|
-    t.binary "key", limit: 1024, null: false
-    t.binary "value", limit: 536870912, null: false
-    t.datetime "created_at", null: false
-    t.integer "key_hash", limit: 8, null: false
-    t.integer "byte_size", limit: 4, null: false
-    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
-    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
-    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   add_foreign_key "feeds", "folders"
